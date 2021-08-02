3e83044312ab36a4244bcf3e06c268b25b7b0517
Ivgesha <evgenise93@gmail.com>
2021-08-02 18:37:46 +0300

    Added "slider" to recommendations

--- src/components/RecommendationSlider.vue
+++ src/components/RecommendationSlider.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="slide-recommandation">
-    <div class="slide-recommandation-container" :data="recommendation">
+    <div class="slide-recommandation-container" :data="dataObj.recommendation">
       <div class="slide-recommandation-container--RecTitle">
         Recommendations
       </div>
       <div>
         <img
           class="slide-recommandation-container--recPicture"
-          :src="recommendation[recIndex].recImg"
-          :alt="recommendation[recIndex].recAlt"
+          :src="dataObj.recommendation[dataObj.recIndex].recImg"
+          :alt="dataObj.recommendation[dataObj.recIndex].recAlt"
         />
       </div>
       <div class="slide-recommandation-container--recName">
-        <span>{{ recommendation[recIndex].recName }}</span>
+        <span>{{ dataObj.recommendation[dataObj.recIndex].recName }}</span>
       </div>
       <div class="slide-recommandation-container--recPos">
-        <span>{{ recommendation[recIndex].recPos }}</span>
+        <span>{{ dataObj.recommendation[dataObj.recIndex].recPos }}</span>
       </div>
       <div class="slide-recommandation-container--recPar">
-        <p>{{ recommendation[recIndex].recPar }}</p>
+        <p>{{ dataObj.recommendation[dataObj.recIndex].recPar }}</p>
       </div>
       <div>
-        <span class="slideLocation">loc</span>
+        <span class="slideLocation">{{ dataObj.recommendation.recIndex }}</span>
       </div>
     </div>
   </div>
@@ -34,27 +34,55 @@ export default {
   name: "RecommendationSlider",
   data: () => {
     return {
-      recommendation: [
-        {
-          recImg:
-            "https://media-exp1.licdn.com/dms/image/C4D03AQHnyXHat4qRHg/profile-displayphoto-shrink_100_100/0/1601020654678?e=1632960000&v=beta&t=CUH1Qpxx7GN0iwnOJFu0ZWYrKCvY_4f__B1FvAV3p0s",
-          recAlt: "Gil",
-          recName: "GiL Nagar",
-          recPos: "System And Network Manager at 019Mobile",
-          recPar:
-            '" Evgeni is very talented guy with excellent learning skills and great teamwork player, I have the pleasure to work with him on different projects and learning from his knowledge. He is happy to assist in any time and know how to push things forward, on top all of that Evgeni is reliable and fun guy to work with him. "',
-        },
-        {
-          recImg:
-            "https://media-exp1.licdn.com/dms/image/C4E03AQHX9InGA_SPuw/profile-displayphoto-shrink_100_100/0/1549802056022?e=1632960000&v=beta&t=asJljt3ZJVoKsqKdG2_-y6hVBn48sF_WC8m8CfOZflk",
-          recAlt: "Eliyahu Kriel",
-          recPos: "Frontend Developer at Devalore",
-          recPar:
-            '" I worked with Evgeni at 019 Mobile for almost a year. Evgeni is a very talented, smart and creative Developer. Always accepted the complex tasks, and made sure to do them in the best way with a very impressive level of performance. Evgeni is excellent at teamwork, professional dialogue, mutual help, always shares the knowledge he has, and if he does not know something - he is not ashamed to ask - and learns very quickly. I recommend Evgeni as a professional Developer, is in constant learning, smart and thorough. "',
-        },
-      ],
-      recIndex: 0,
+      dataObj: {
+        recommendation: [
+          {
+            recImg:
+              "https://media-exp1.licdn.com/dms/image/C4D03AQHnyXHat4qRHg/profile-displayphoto-shrink_100_100/0/1601020654678?e=1632960000&v=beta&t=CUH1Qpxx7GN0iwnOJFu0ZWYrKCvY_4f__B1FvAV3p0s",
+            recAlt: "Gil",
+            recName: "GiL Nagar",
+            recPos: "System And Network Manager at 019Mobile",
+            recPar:
+              '" Evgeni is very talented guy with excellent learning skills and great teamwork player, I have the pleasure to work with him on different projects and learning from his knowledge. He is happy to assist in any time and know how to push things forward, on top all of that Evgeni is reliable and fun guy to work with him. "',
+          },
+          {
+            recImg:
+              "https://media-exp1.licdn.com/dms/image/C4E03AQHX9InGA_SPuw/profile-displayphoto-shrink_100_100/0/1549802056022?e=1632960000&v=beta&t=asJljt3ZJVoKsqKdG2_-y6hVBn48sF_WC8m8CfOZflk",
+            recAlt: "Eliyahu Kriel",
+            recPos: "Frontend Developer at Devalore",
+            recPar:
+              '" I worked with Evgeni at 019 Mobile for almost a year. Evgeni is a very talented, smart and creative Developer. Always accepted the complex tasks, and made sure to do them in the best way with a very impressive level of performance. Evgeni is excellent at teamwork, professional dialogue, mutual help, always shares the knowledge he has, and if he does not know something - he is not ashamed to ask - and learns very quickly. I recommend Evgeni as a professional Developer, is in constant learning, smart and thorough. "',
+          },
+        ],
+        recIndex: 0,
+      },
     };
+  },
+  props: {
+    recIndex: Number,
+  },
+  methods: {
+    nextSlide() {
+      // maybe use this.$set(this.dataObj, "recIndex", temp);
+      // or just use  this.dataObj.recIndex; + 1;
+      /* 
+      let temp = this.dataObj.recIndex;
+      temp += 1;
+      // console.log("recIndex: " + recIndex);
+      this.$set(this.dataObj, "recIndex", temp);
+      */
+      if (this.dataObj.recIndex + 1 == this.dataObj.recommendation.length) {
+        this.dataObj.recIndex = 0;
+      } else {
+        this.dataObj.recIndex += 1;
+      }
+    },
+  },
+
+  mounted() {
+    this.interval = setInterval(() => {
+      this.nextSlide();
+    }, 10000);
   },
 };
 </script>
