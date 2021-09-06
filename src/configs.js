c3306b6327a78207e09807170e218385f3f1905c
Ivgesha <evgenise93@gmail.com>
2021-09-06 04:41:07 +0300

    cv linker

--- src/configs.js
+++ src/configs.js
@@ -2,4 +2,4 @@ export const EmailLinker = "mailto:evgeniseev@gmail.com";
 export const GithubLinker = "https://github.com/Ivgesha";
 export const LinkedInLinker = "https://www.linkedin.com/in/evgeni-iseev-622850181/";
 export const FacebookLinker = "https://www.facebook.com/Ivgesha93";
-export const ResumeLinker = "https://drive.google.com/file/d/1jgGKVWYR_L_qLaXNpCdXmqilmKFAkDV7/view?usp=sharing";
+export const ResumeLinker = "https://drive.google.com/file/d/1_mtxkG3qgBKfpCNc2eXTCWZ07_OBFbF4/view?usp=sharing";
